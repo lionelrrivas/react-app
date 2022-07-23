819ca3b82ea28b179159751f1c3777d2a07ddbca
lionelrrivas <lionel.r.rivas@gmail.com>
2022-07-23 14:40:03 -0500

    feat: adding another feature.  (#21)
    * feat: added a new feature
    * feat: feature added for release

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to my React App 2
+          Welcome to my React App version 5
         </a>
       </header>
     </div>
