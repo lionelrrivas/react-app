23569f57bf1966c8f7147d8ad297b9cc28e84414
lionelrrivas <lionel.r.rivas@gmail.com>
2022-07-23 14:47:19 -0500

    feat: feature for version 6

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders code in learn link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to my React App version 5/i);
+  const linkElement = screen.getByText(/Welcome to my React App version 6/i);
   expect(linkElement).toBeInTheDocument();
 });
