ee51527818c9252ac7245752914691656b1358d4
lionelrrivas <lionel.r.rivas@gmail.com>
2022-07-23 14:24:40 -0500

    feat: feature added for release

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders code in learn link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to my React App 2/i);
+  const linkElement = screen.getByText(/Welcome to my React App version 5/i);
   expect(linkElement).toBeInTheDocument();
 });
