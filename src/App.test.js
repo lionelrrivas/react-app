112c1475d9a250a7de936f50e8c67bef77be7bdd
lionelrrivas <lionel.r.rivas@gmail.com>
2022-09-17 15:51:49 -0500

    feat: update welcome page version to 11

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders code in learn link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to my React App version 10/i);
+  const linkElement = screen.getByText(/Welcome to my React App version 11/i);
   expect(linkElement).toBeInTheDocument();
 });
