a0d78fa4dc6dbee5ba11828a381379f8a2a0849a
lionelrrivas <lionel.r.rivas@gmail.com>
2022-09-24 15:18:19 -0500

    fix: fix broken unit test
    fix broken unit test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders code in learn link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to my React App version 14/i);
+  const linkElement = screen.getByText(/Welcome to my React App/i);
   expect(linkElement).toBeInTheDocument();
 });
