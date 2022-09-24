d90dd1255b5de4661b5a6cac07007cd68b620d27
lionelrrivas <lionel.r.rivas@gmail.com>
2022-09-24 14:11:46 -0500

    feat: add automated issue creation
    add automated issue creation

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to my React App version 14
+          Welcome to my React App
         </a>
       </header>
     </div>
