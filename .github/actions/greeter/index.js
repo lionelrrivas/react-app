6cc67069f8140e03f1d828652828683549d18757
lionelrrivas <lionel.r.rivas@gmail.com>
2022-12-05 06:52:50 -0600

    style: fix code formatting

--- .github/actions/greeter/index.js
+++ .github/actions/greeter/index.js
@@ -1,14 +1,14 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-    const personToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${personToGreet}`);
-    
-    const time = new Date();
-    core.setOutput("time", time.toDateString());
-    
-    console.log(JSON.stringify(github, null, '\t'));
+  const personToGreet = core.getInput("who-to-greet");
+  console.log(`Hello ${personToGreet}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toDateString());
+
+  console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
