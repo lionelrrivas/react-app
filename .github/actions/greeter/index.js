6b6528f73813348e6af85bd7921b411a1c6f8454
lionelrrivas <lionel.r.rivas@gmail.com>
2022-12-31 15:53:42 -0600

    feat: demo actions core package

--- .github/actions/greeter/index.js
+++ .github/actions/greeter/index.js
@@ -2,13 +2,22 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  core.debug("debug message");
+  core.warning("warning message");
+  core.error("error message");
+
   const personToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${personToGreet}`);
+  core.setSecret(personToGreet);
 
   const time = new Date();
   core.setOutput("time", time.toDateString());
 
+  core.startGroup("Loggin github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
