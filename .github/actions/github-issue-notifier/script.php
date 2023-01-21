af1f55da2c627eb5c55b07c2726c2e64fab89b29
lionelrrivas <lionel.r.rivas@gmail.com>
2023-01-21 15:47:06 -0600

    feat: adding a slack issue notifier

---
+++ .github/actions/github-issue-notifier/script.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'vendor/autoload.php';
+Requests::register_autoloader();
+
+var_dump($argv);
+var_dump($_ENV);
+// $response = Requests::post(
+//     "https://hooks.slack.com/services/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",
+//     array(
+//         'Content-type' => 'application/json'
+//     ),
+//     json_encode(array(
+//         'blocks' => 
+//             array(
+//                 array(
+//                 "type" => "section",
+//                 "text" => array(
+//                     "type" => "mrkdwn",
+//                     "text" => "You have a new request: *<fakelink.toemployeeprofile.com|Fred Enriquez - New device request>*",
+//                 ),
+//             ),
+//             array(
+//                 "type" => "section",
+//                 "fields" => array(
+//                     array(
+//                         "type" => "mrkdwn",
+//                         "text" => "*When:*\nComputer (laptop)",
+//                     ),
+//                     array(
+//                         "type" => "mrkdwn",
+//                         "text" => "*When:*\nSubmitted Aug 10",
+//                     ),
+//                 ),
+//             ),
+//         ),
+//     ))
+// );
+
+// var_dump($response);
+
+if(!$response->success) {
+    echo $response->body;
+}
