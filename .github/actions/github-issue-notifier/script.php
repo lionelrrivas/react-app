e78624d6b3b9b3d4cdc52c3b09d4b22fd51c7ed9
lionelrrivas <lionel.r.rivas@gmail.com>
2023-02-18 15:16:45 -0600

    finish php github docker action to send slack notifications from github

--- .github/actions/github-issue-notifier/script.php
+++ .github/actions/github-issue-notifier/script.php
@@ -5,39 +5,47 @@ Requests::register_autoloader();
 
 var_dump($argv);
 var_dump($_ENV);
-// $response = Requests::post(
-//     "",
-//     array(
-//         'Content-type' => 'application/json'
-//     ),
-//     json_encode(array(
-//         'blocks' => 
-//             array(
-//                 array(
-//                 "type" => "section",
-//                 "text" => array(
-//                     "type" => "mrkdwn",
-//                     "text" => "You have a new request: *<fakelink.toemployeeprofile.com|Fred Enriquez - New device request>*",
-//                 ),
-//             ),
-//             array(
-//                 "type" => "section",
-//                 "fields" => array(
-//                     array(
-//                         "type" => "mrkdwn",
-//                         "text" => "*When:*\nComputer (laptop)",
-//                     ),
-//                     array(
-//                         "type" => "mrkdwn",
-//                         "text" => "*When:*\nSubmitted Aug 10",
-//                     ),
-//                 ),
-//             ),
-//         ),
-//     ))
-// );
+$response = Requests::post(
+    $_ENV('INPUT_SLACK_WEBHOOK'),
+    array(
+        'Content-type' => 'application/json'
+    ),
+    json_encode(array(
+        'blocks' => 
+            array(
+                array(
+                "type" => "section",
+                "text" => array(
+                    "type" => "mrkdwn",
+                    "text" => $_ENV['INPUT_MESSAGE'],
+                ),
+            ),
+            array(
+                "type" => "section",
+                "fields" => array(
+                    array(
+                        "type" => "mrkdwn",
+                        "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+                    ),
+                    array(
+                        "type" => "mrkdwn",
+                        "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
+                    ),
+                    array(
+                        "type" => "mrkdwn",
+                        "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}"
+                    ),
+                    array(
+                        "type" => "mrkdwn",
+                        "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+                    ),
+                ),
+            ),
+        ),
+    ))
+);
 
-// var_dump($response);
+var_dump($response);
 
 if(!$response->success) {
     echo $response->body;
